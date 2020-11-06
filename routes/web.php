cd3baa6f495521cc644255bdb13a01b89cca0127
Kanwal <knwltanveer640@gmail.com>
2020-11-06 19:16:14 +0500

    laravel-workspace

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route::post('login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
-// // Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
